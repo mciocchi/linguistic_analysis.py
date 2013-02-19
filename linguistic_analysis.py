20864eaa5e83dd466544fd9011a40e611e0b7d80
Matt Ciocchi <mciocchi@gmail.com>
2013-02-19 17:16:59 -0500

    Added a docstring. Changed function to output a list of dictionaries for ease of processing.

--- linguistic_analysis.py
+++ linguistic_analysis.py
@@ -1,9 +1,28 @@
+# linguistic_analysis.py
+# by Matt Ciocchi
+# last updated 2/19/2013
+
 def getwordfrequencies(textfield):
-	word_frequencies= {}
+	"""
+	getwordfrequencies is designed to take in a string and output a list of dictionaries recording each unique word, and how many times it's used. 
+
+	for instance:
+
+	getwordfrequencies('foo foo bar')
+
+	will return:
+
+	[{'word': 'foo', 'word_count': 2}, {'word': 'bar', 'word_count': 1}]
+	"""
 
 #	Turn text field into a list of words:
 	textfield= textfield.split()
+
+#	Convert the list into a set to filter out all but one instance of each unique word.
+#	Afterwards, convert the set back into a list in order to be able to iterate through it.
 	lexicon= list(set(textfield))
+
+	word_frequencies= []
 
 #	Iterate through each word in the list of words:
 	for word in lexicon:
@@ -16,7 +35,7 @@ def getwordfrequencies(textfield):
 		word_count= len(filter(bool, map(matchesword, textfield)))
 
 #		The following should associate words and word counts as key/value pairs in a dictionary:
-		word_frequencies.update({word : word_count,})
+		word_frequencies.append({'word':word,'word_count':word_count,})
 
 #	return dictionary of words and word counts, e.g. {'foo':3, 'bar': 4, 'foobar': 5,}
 	return word_frequencies
